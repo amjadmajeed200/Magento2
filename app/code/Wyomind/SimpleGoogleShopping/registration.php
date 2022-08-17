6ecfdaa87b5bf30d03355061299d01b7db39c457
Anees Ahmad <aneesahmadweb@gmail.com>
2022-08-18 03:38:34 +0500

    core

--- app/code/Wyomind/SimpleGoogleShopping/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Wyomind_SimpleGoogleShopping',
-    __DIR__
-);
