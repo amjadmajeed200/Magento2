7efa448c27cab71072dbd96dce40ffd34d08d3ad
Anees Ahmad <aneesahmadweb@gmail.com>
2022-08-29 17:56:31 +0500

    wholesale

---
+++ app/code/Cooperative/Customwork/Block/Wholesale.php
@@ -0,0 +1,11 @@
+<?php
+namespace Cooperative\Customwork\Block;
+
+class Wholesale extends  \Magento\Framework\View\Element\Template
+{
+
+    public function getPro()
+    {
+        echo 'a';
+    }
+}
